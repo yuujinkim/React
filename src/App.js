a4386aeca7ae02d8716d62be556148ce02bbd725
yuujinkim <yuuuuu01@naver.com>
2021-08-30 18:30:54 +0900

    1-24. 클래스형 컴포넌트

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import React, { useRef, useReducer, useMemo, useCallback } from 'react';
+import React, { useReducer, useMemo } from 'react';
 import UserList from './UserList';
 import CreateUser from './CreateUser';
-import useInputs from './hooks/useInputs';
+import produce from 'immer';
 
 function countActiveUsers(users) {
   console.log('활성 사용자 수를 세는중...');
@@ -34,21 +34,19 @@ const initialState = {
 function reducer(state, action) {
   switch (action.type) {
     case 'CREATE_USER':
-      return {
-        users: state.users.concat(action.user)
-      };
+      return produce(state, draft => {
+        draft.users.push(action.user);
+      });
     case 'TOGGLE_USER':
-      return {
-        ...state,
-        users: state.users.map(user =>
-          user.id === action.id ? { ...user, active: !user.active } : user
-        )
-      };
+      return produce(state, draft => {
+        const user = draft.users.find(user => user.id === action.id);
+        user.active = !user.active;
+      });
     case 'REMOVE_USER':
-      return {
-        ...state,
-        users: state.users.filter(user => user.id !== action.id)
-      };
+      return produce(state, draft => {
+        const index = draft.users.findIndex(user => user.id === action.id);
+        draft.users.splice(index, 1);
+      });
     default:
       return state;
   }
@@ -58,37 +56,14 @@ function reducer(state, action) {
 export const UserDispatch = React.createContext(null);
 
 function App() {
-  const [{ username, email }, onChange, onReset] = useInputs({
-    username: '',
-    email: ''
-  });
   const [state, dispatch] = useReducer(reducer, initialState);
-  const nextId = useRef(4);
 
   const { users } = state;
-
-  const onCreate = useCallback(() => {
-    dispatch({
-      type: 'CREATE_USER',
-      user: {
-        id: nextId.current,
-        username,
-        email
-      }
-    });
-    onReset();
-    nextId.current += 1;
-  }, [username, email, onReset]);
 
   const count = useMemo(() => countActiveUsers(users), [users]);
   return (
     <UserDispatch.Provider value={dispatch}>
-      <CreateUser
-        username={username}
-        email={email}
-        onChange={onChange}
-        onCreate={onCreate}
-      />
+      <CreateUser />
       <UserList users={users} />
       <div>활성사용자 수 : {count}</div>
     </UserDispatch.Provider>
--- src/Hello.js
+++ src/Hello.js
@@ -1,16 +1,18 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-function Hello({ color, name, isSpecial }) {
-  return (
-    <div style={{ color }}>
-      {isSpecial && <b>*</b>}
-      안녕하세요 {name}
-    </div>
-  );
-}
-
-Hello.defaultProps = {
-  name: '이름없음'
+class Hello extends Component {
+  static defaultProps = {
+    name: '이름없음'
+  };
+  render() {
+    const { color, name, isSpecial } = this.props;
+    return (
+      <div style={{ color }}>
+        {isSpecial && <b>*</b>}
+        안녕하세요 {name}
+      </div>
+    );
+  }
 }
 
 export default Hello;

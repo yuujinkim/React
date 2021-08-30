fdd28076f491b29be77f6892c729b63fe1c36b4f
yuujinkim <yuuuuu01@naver.com>
2021-08-30 18:49:56 +0900

    1-26. componentDidCatch 로 에러 잡아내기 / Sentry 연동

--- src/App.js
+++ src/App.js
@@ -1,72 +1,16 @@
-import React, { useReducer, useMemo } from 'react';
-import UserList from './UserList';
-import CreateUser from './CreateUser';
-import produce from 'immer';
-
-function countActiveUsers(users) {
-  console.log('활성 사용자 수를 세는중...');
-  return users.filter(user => user.active).length;
-}
-
-const initialState = {
-  users: [
-    {
-      id: 1,
-      username: 'velopert',
-      email: 'public.velopert@gmail.com',
-      active: true
-    },
-    {
-      id: 2,
-      username: 'tester',
-      email: 'tester@example.com',
-      active: false
-    },
-    {
-      id: 3,
-      username: 'liz',
-      email: 'liz@example.com',
-      active: false
-    }
-  ]
-};
-
-function reducer(state, action) {
-  switch (action.type) {
-    case 'CREATE_USER':
-      return produce(state, draft => {
-        draft.users.push(action.user);
-      });
-    case 'TOGGLE_USER':
-      return produce(state, draft => {
-        const user = draft.users.find(user => user.id === action.id);
-        user.active = !user.active;
-      });
-    case 'REMOVE_USER':
-      return produce(state, draft => {
-        const index = draft.users.findIndex(user => user.id === action.id);
-        draft.users.splice(index, 1);
-      });
-    default:
-      return state;
-  }
-}
-
-// UserDispatch 라는 이름으로 내보내줍니다.
-export const UserDispatch = React.createContext(null);
+import React from 'react';
+import User from './User';
+import ErrorBoundary from './ErrorBoundary';
 
 function App() {
-  const [state, dispatch] = useReducer(reducer, initialState);
-
-  const { users } = state;
-
-  const count = useMemo(() => countActiveUsers(users), [users]);
+  const user = {
+    id: 1,
+    username: 'velopert'
+  };
   return (
-    <UserDispatch.Provider value={dispatch}>
-      <CreateUser />
-      <UserList users={users} />
-      <div>활성사용자 수 : {count}</div>
-    </UserDispatch.Provider>
+    <ErrorBoundary>
+      <User />
+    </ErrorBoundary>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-// import App from './App';
+import App from './App';
+import * as Sentry from '@sentry/browser';
 import * as serviceWorker from './serviceWorker';
-import Counter from './Counter';
 
-ReactDOM.render(<Counter />, document.getElementById('root'));
+Sentry.init({
+  dsn: "https://0b9cc30282fc4a1bbfe0bcd1a039e108@o982191.ingest.sentry.io/5937063"
+});
+
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
